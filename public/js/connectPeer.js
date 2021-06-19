4c9545d45c03dda1169ea3f2813855bc6460eed3
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-06-19 06:57:40 +0530

    toggling of user audio and video stream implemented

--- public/js/connectPeer.js
+++ public/js/connectPeer.js
@@ -1,4 +1,6 @@
 const videoGrid = document.getElementById('video-grid');
+const micToggleBtn = document.querySelector(".mic");
+const videoToggleBtn = document.querySelector(".user-video");
 
 // setting up the connection to the peer server
 // first param is undefined because i want the server to take care of generating a new id
@@ -9,6 +11,8 @@ const peer = new Peer(undefined, {
 })
 // peer server is going to take all of the webrtc information of a user and generate a unique client id for that user, which can be used to connect with other peers on the network
 
+let myScreen = null;
+let myVideoStream;
 const mypeers = {}
 const myVideo = document.createElement('video');
 myVideo.muted = true; // because we don't want to listen to our own video
@@ -17,14 +21,16 @@ navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => { // stream -> our video and our audio
+    myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
+    // runs when reciever recieves call from sender
     peer.on('call', call => {
         call.answer(stream);   
         const video = document.createElement('video')
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
-        }) 
+        })
     })
 
     socket.on('user-connected', userId => {
@@ -65,5 +71,49 @@ function addVideoStream(video, stream){
         video.play();
     })
     // adding video to our video-grid
-    videoGrid.append(video);
+    videoGrid.insertBefore(video, videoGrid.querySelector(".video-tool__container"));
 }
+
+function screenSharing() {
+    navigator.mediaDevices.getDisplayMedia().then(stream => {
+    myScreen = stream
+    const keys = Object.keys(mypeers)
+    keys.forEach((value) => {
+        peer.call(value, myScreen)
+    });
+    })
+}
+
+micToggleBtn.addEventListener("click", function(){
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+
+        let icon = micToggleBtn.querySelector(".fas");
+        icon.classList.remove("fa-microphone-alt");
+        icon.classList.add("fa-microphone-alt-slash")
+    } else {
+        myVideoStream.getAudioTracks()[0].enabled = true;
+
+        let icon = micToggleBtn.querySelector(".fas");
+        icon.classList.remove("fa-microphone-alt-slash")
+        icon.classList.add("fa-microphone-alt");
+    }
+})
+
+videoToggleBtn.addEventListener("click", function(){
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+
+        let icon = videoToggleBtn.querySelector(".fas");
+        icon.classList.remove("fa-video");
+        icon.classList.add("fa-video-slash")
+    } else {
+        myVideoStream.getVideoTracks()[0].enabled = true;
+
+        let icon = videoToggleBtn.querySelector(".fas");
+        icon.classList.remove("fa-video-slash")
+        icon.classList.add("fa-video");
+    }
+})
